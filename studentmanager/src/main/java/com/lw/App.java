3dd8137ead8cd34835b16d48cf19ed11e1bc0aa0
wuyujun <wuyj@tranzvision.com.cn>
2020-12-16 21:37:06 +0800

    解决jar版本不匹配

--- studentmanager/src/main/java/com/lw/App.java
+++ studentmanager/src/main/java/com/lw/App.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author yujun.wu
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.lw")
+@EnableSwagger2
 @Slf4j
 public class App {
     public static void main(String[] args) {
